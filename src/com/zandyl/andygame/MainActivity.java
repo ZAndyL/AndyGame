ce1aeffe55a83b4d2517873975db8595ec470ddc
Andy Liang <z32liang@uwaterloo.ca>
2014-06-07 13:45:45 -0400

    unregisters sensor in surfacedestroyed

--- src/com/zandyl/andygame/MainActivity.java
+++ src/com/zandyl/andygame/MainActivity.java
@@ -62,6 +62,11 @@ public class MainActivity extends Activity {
 		
 	}
 	
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+	
 	
 	private class MainGameView extends SurfaceView implements
 			SurfaceHolder.Callback, SensorEventListener{
@@ -175,7 +180,7 @@ public class MainActivity extends Activity {
 			soundID = soundPool.load(getApplicationContext(), R.raw.oddbounce,
 					1);
 		}
-
+		
 		@Override
 		public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2,
 				int arg3) {
@@ -253,6 +258,8 @@ public class MainActivity extends Activity {
 		public void surfaceDestroyed(SurfaceHolder holder) {
 			if (null != mDrawingThread)
 				mDrawingThread.interrupt();
+			
+			sensorManager.unregisterListener(gameView);
 		}
 
 		
